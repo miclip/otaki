6f6843b0203d320f2fdc1848b3a65538c59636fc
Michael Lipscombe <mlipscombe@outlook.com>
2017-09-29 08:59:05 -0600

    Add repositories

---
+++ src/main/java/com/miclip/repository/SessionRepository.java
@@ -0,0 +1,8 @@
+package com.miclip.repository;
+
+import com.miclip.entity.Session;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SessionRepository extends InMemoryRepository<Session> {
+}
